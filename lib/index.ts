0e2b5402c84a1bd194598f46be8a043e227ba207
Cory Mickelson <cmickelson@skyslope.com>
2017-10-12 08:03:23 -0700

    fix exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,34 @@
-import * as doc from './document'
-import * as dict from './dictionary'
-import * as obj from './object'
-import * as page from './page'
-import * as annot from './annotation'
-import * as fields from './field'
-import * as image from './image'
-import * as painter from './painter'
-import * as rect from './rect'
-import * as sign from './signature'
+import {Document as doc} from './document'
+import {Dictionary as dict} from './dictionary'
+import {Obj as obj} from './object'
+import {Page as page} from './page'
+import {
+    Annotation as annot,
+    NpdfAction as action,
+    NPdfAnnotation as annotenum,
+    NpdfAnnotationFlag as annotflags,
+    NPdfAnnotationType as annottype
+} from './annotation'
+import {CheckBox as checkbox, Field as field, TextField as textfield} from './field'
+import {Image as image} from './image'
+import {Painter as painter} from './painter'
+import {Rect as rect} from './rect'
+import {Signature as sign} from './signature'
 
 export const Document = doc,
     Page = page,
     Dictionary = dict,
     Obj = obj,
     Annotation = annot,
-    Field = fields,
+    NPdfAction = action,
+    NPdfAnnotation = annotenum,
+    NPdfAnnotationFlag = annotflags,
+    NPdfAnnotationType = annottype,
+    Field = field,
+    CheckBox = checkbox,
+    TextField = textfield,
     Image = image,
     Painter = painter,
     Rect = rect,
     Signature = sign
+
