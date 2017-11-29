4d18c52cdcd3dd44c9e8a0db7e15575634b06209
Cory Mickelson <mickelsoncory@gmail.com>
2017-11-29 06:43:27 -0800

    build shared

--- alpine-install-deps.sh
+++ alpine-install-deps.sh
@@ -2,6 +2,7 @@ git clone https://github.com/svn2github/podofo $HOME/podofo && \
 cd $HOME/podofo && \
 mkdir $HOME/podofo/build && \
 cmake -DCMAKE_INSTALL_PREFIX=/usr $HOME/podofo/ \
+-DPODOFO_BUILD_SHARED=1 \
 -DPODOFO_HAVE_PNG_LIB=1 \
 -DPODOFO_HAVE_JPEG_LIB=1 \
 -DPODOFO_HAVE_TIFF_LIB=1 \
@@ -12,4 +13,4 @@ make install && \
 git clone https://github.com/corymickelson/NoPoDoFo $HOME/npdf && \
 cd $HOME/npdf && \
 npm i && \
-npm run compile && npm run build-lib && npm test
+npm run compile && npm run lib-build && npm test
