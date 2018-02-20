d74d622828971da3f391ec3be346cf68ed0d37e9
Cory Mickelson <mickelsoncory@gmail.com>
2018-02-20 10:34:09 -0800

    copyright file header

--- lib/image.ts
+++ lib/image.ts
@@ -1,4 +1,22 @@
-// const mod = require('../src/npdf')
+/**
+ * This file is part of the NoPoDoFo (R) project.
+ * Copyright (c) 2017-2018
+ * Authors: Cory Mickelson, et al.
+ * 
+ * NoPoDoFo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NoPoDoFo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 import {IDocument, __mod} from './document'
 
