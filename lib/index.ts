28aa579ec1810cb67dfd55d85520a33c1ea8ab2a
Cory Mickelson <mickelsoncory@gmail.com>
2018-04-11 12:56:41 -0700

    fix index.js exports

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@
  * This file is part of the NoPoDoFo (R) project.
  * Copyright (c) 2017-2018
  * Authors: Cory Mickelson, et al.
- * 
+ *
  * NoPoDoFo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Data } from './data'
-import { Document, FontEncoding } from './document'
-import { Obj } from './object'
-import { Page } from './page'
+import {Data} from './data'
+import {Document, FontEncoding} from './document'
+import {Obj} from './object'
+import {Page} from './page'
 import {
     Annotation,
     NPDFAction,
@@ -34,7 +34,7 @@ import {
     ListBox,
     ComboBox
 } from './field'
-import { Image } from './image'
+import {Image} from './image'
 import {
     Painter,
     Encoding,
@@ -51,16 +51,16 @@ import {
     NPDFTextRenderingMode,
     NPDFVerticalAlignment
 } from './painter'
-import { Rect } from './rect'
-import { Signer, signature } from './signer'
-import { Stream } from './stream'
-import { Form } from './form'
-import { ContentsTokenizer } from './parser'
+import {Rect} from './rect'
+import {Signer, signature} from './signer'
+import {Stream} from './stream'
+import {Form} from './form'
+import {ContentsTokenizer} from './parser'
 import {Ref} from './reference'
 import {Cell, Table} from './table'
 
 
-export default {
+export {
     ContentsTokenizer,
     Form,
     Stream,
@@ -99,27 +99,8 @@ export default {
     signature,
     Ref,
     Cell,
-    Table,
+    Table
 }
 export const CONVERSION = 0.0028346456693
-// export const Document = doc,
-//     Page = page,
-//     Obj = obj,
-//     Annotation = annot,
-//     NPdfAction = action,
-//     NPdfAnnotation = annotenum,
-//     NPdfAnnotationFlag = annotflags,
-//     NPdfAnnotationType = annottype,
-//     Field = field,
-//     Form = form,
-//     CheckBox = checkbox,
-//     TextField = textfield,
-//     ListBox = listbox,
-//     ComboBox = combobox,
-//     Image = image,
-//     Painter = painter,
-//     Rect = rect,
-//     Signature = sign,
-//     Stream = stream,
-//     ContentsTokenizer = ct,
+
 
