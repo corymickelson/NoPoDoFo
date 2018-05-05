ce7a5c0627ef764cce72ad5b491cf6cb13fedb6b
Cory Mickelson <mickelsoncory@gmail.com>
2018-05-04 21:38:51 -0700

    add module recursion

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,19 @@
 const {execSync: run} = require('child_process')
 const {join} = require('path')
-const cmd = `cmake-js compile --CDPROJECT_DIR=${join(__dirname, '../../../')} --prefer-clang --s=c++14`
+const {readdirSync} = require('fs')
+
+function findModules(dir = '../', depth = 0) {
+  if (depth >= 4) {
+   throw Error('Depth error') 
+  }
+  let files = readdirSync(dir)
+  if (files.indexOf('node_modules')) {
+    return join(__dirname, dir)
+  } else {
+    return findModules(dir + '../', depth++)
+  }
+}
+
+const cmd = `cmake-js compile --CDPROJECT_DIR=${findModules()} --prefer-clang --s=c++14`
 console.log('CMD: ', cmd)
 run(cmd, {"stdio": "inherit"})
